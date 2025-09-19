b78ba872a0f8cbfb2922094c7bd7279ca154abdc
Kavya Trivedi <kavyatrivedi931@gmail.com>
2025-09-19 22:32:52 +0530

    Error hai

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -19,12 +19,7 @@ function Home() {
     }
 
     return (
-
-        <div className="pt-20 md:pt-25 p-4 bg-gradient-to-b from-sky-200 to-gray-500 min-h-screen md:pl-54">
-
         <div className="pt-22 md:pt-25 p-2 md:px-10 lg:px-40 bg-gradient-to-b from-sky-200 to-gray-500 min-h-screen">
-
-
             <div className="bg-white rounded-lg shadow py-6 p-4 mb-6">
                 <h1 className="text-3xl font-semibold italic">
                     {/* 3. Use the user's name dynamically */}
@@ -37,7 +32,6 @@ function Home() {
 
             {/* Cards */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-6">
-
                 <div className="bg-white rounded-lg shadow p-4 text-center">
                     <p className="text-gray-700 font-bold">My Petitions</p>
                     <h2 className="text-2xl font-bold">0</h2>
@@ -62,25 +56,36 @@ function Home() {
                 <h2 className="text-lg font-semibold border-b-2">Active Petitions Near You</h2>
 
                 <p className="text-gray-900 text-md flex">Showing for:
-                    <span className="bg-gray-500 font-semibold  px-2.5 rounded-lg ml-1">
-                        <select className="bg-gray-500 text-white font-semibold rounded-lg px-1 cursor-pointer ">
+                    <span className="bg-gray-500 font-semibold px-2.5 rounded-lg ml-1">
+                        <select className="bg-gray-500 text-white font-semibold rounded-lg px-1 cursor-pointer">
                             {cities.map((city, index) => (
                                 <option key={index} value={city}>
                                     {city}
                                 </option>
                             ))}
                         </select>
-                    </span></p>
+                    </span>
+                </p>
             </div>
 
-            <div className=" flex flex-wrap gap-4" >
+            <div className="flex flex-wrap gap-4">
                 {categories.map((categori) =>
-                    (<div className={`border text-[16px] rounded-2xl  px-4 py-1 text-center hover:bg-gray-400 hover:font-medium cursor-pointer ${activeCategoriy === categori ? "bg-gray-500 text-white border-black" : "bg-white border-gray-300"}`} key={categori} onClick={() => activeCategori(categori)}>{categori}</div>)
+                    (<div 
+                        className={`border text-[16px] rounded-2xl px-4 py-1 text-center hover:bg-gray-400 hover:font-medium cursor-pointer ${activeCategoriy === categori ? "bg-gray-500 text-white border-black" : "bg-white border-gray-300"}`} 
+                        key={categori} 
+                        onClick={() => activeCategori(categori)}>
+                        {categori}
+                    </div>)
                 )}
             </div>
+            
             <div className='flex justify-center flex-wrap py-6'>
-                <p className='w-full text-center text-2xl font-mono py-2'>no petion found...............</p>
-                <button className='border px-2 text-xl bg-white rounded-md cursor-pointer hover:bg-gray-400' onClick={() => setActiveCategori("All Categories")}>Clear Filter</button>
+                <p className='w-full text-center text-2xl font-mono py-2'>no petition found...............</p>
+                <button 
+                    className='border px-2 text-xl bg-white rounded-md cursor-pointer hover:bg-gray-400' 
+                    onClick={() => setActiveCategori("All Categories")}>
+                    Clear Filter
+                </button>
             </div>
         </div>
     )
